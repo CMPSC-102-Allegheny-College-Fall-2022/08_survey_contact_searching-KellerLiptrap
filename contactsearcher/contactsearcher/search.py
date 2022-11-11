8a701f92da9a32c907f28f47bfa2c705237d8ee3
Keller Liptrap <kellerliptrap@Kellers-MacBook-Pro.local>
2022-11-11 13:49:39 -0500

    code finished

--- contactsearcher/contactsearcher/search.py
+++ contactsearcher/contactsearcher/search.py
@@ -9,7 +9,6 @@ import csv
 
 def search_for_email_given_job(job_description: str, contacts: str) -> List[List[str]]:
     """Search for and return job description(s) given an email address."""
-    # TODO: create an empty list of the contacts
     contacts_list = []
     for contact_line in csv.reader(contacts.splitlines(),quotechar='"',delimiter=",",
     quoting=csv.QUOTE_ALL,
@@ -19,10 +18,3 @@ def search_for_email_given_job(job_description: str, contacts: str) -> List[List
         if job_description in current_contact_job.lower():
             contacts_list.append(contact_line)
     return contacts_list
-    # TODO: iterate through the file, parsing it line by line
-    # TODO: refer to the file called input/contacts.txt to learn more about
-    # the format of the comma separated value (CSV) file that we must parse
-    # TODO: iterate through each line of the file and extract the current job
-    # ---> TODO: extract the current job for the contact on this line of the CSV
-    # ---> TODO: the job description matches and thus we should save it in the list
-    # TODO: return the list of the contacts who have a job description that matches
